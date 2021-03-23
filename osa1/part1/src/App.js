b613d2813482d77a3909f9e93ce5edce943a89ba
kari.tapani.leskinen@gmail.com <kari.tapani.leskinen@gmail.com>
2021-03-23 10:50:56 +0200

    ennen c osaa

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -1,17 +1,51 @@
 import React, { useState } from 'react'
 
+/*
+const Display = ({ counter }) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+Koska Display komponentti vain palauttaa counter muuttujan arvon
+voidaan se määrittää nuolifunktiolla yhdellä rivillä
+*/
+const Display = ({ counter }) => <div>{counter}</div>
+
+// eli jos näkee tälläistä eikä propseja missään niin ne on
+// dekunstruktoitu, destructured
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = (props) => {
   // counter == variable of the state
   // setCounter == functio jolla muutetaan tilaa
   const [counter, setCounter] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />   
+  </div>
   )
 }
 
