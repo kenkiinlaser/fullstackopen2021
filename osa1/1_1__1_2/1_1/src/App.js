5018452d194e5112dc0d4551e689edd0602cb502
kari.tapani.leskinen@gmail.com <kari.tapani.leskinen@gmail.com>
2021-05-31 14:44:33 +0300

    1.7

--- osa1/1_1__1_2/1_1/src/App.js
+++ osa1/1_1__1_2/1_1/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 
-const Statistics = ({good, neutral, bad}) => {
+const Statistics = ({good, neutral, bad, all, positive, average}) => {
   return (
     <div>
       <h1>Statistics</h1>
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>positive: {(positive / all) * 100} %</p>
+      <p>average: {average / all}</p>
     </div>
   )
 }
@@ -22,11 +25,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-
-  const handleSetGood = () => setGood(good + 1)
-  const handleSetNeutral = () => setNeutral(neutral + 1)
-  const handleSetBad = () => setBad(bad + 1)
+  const handleSetGood = () => {
+    setAll(all + 1)
+    setAverage(average + 1)
+    setGood(good + 1)
+    setPositive(positive + 1)
+  }
+  const handleSetNeutral = () => {
+    setAll(all + 1)
+    setAverage(average + 0)
+    setNeutral(neutral + 1)
+  }
+  const handleSetBad = () => {
+    setAll(all + 1)
+    setAverage(average - 1)
+    setBad(bad + 1)
+  }
 
   return (
     <div>
@@ -34,7 +52,7 @@ const App = () => {
       <Button handleClick={handleSetGood} text='good' />
       <Button handleClick={handleSetNeutral} text='neutral' />
       <Button handleClick={handleSetBad} text='bad' />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}/>
     </div>
 
   )
